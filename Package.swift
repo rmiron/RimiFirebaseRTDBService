995b87c7a7de80e493ca34f1927be875fa06f910
Ricardo Miron <rmiron1@socal.rr.com>
2025-11-12 06:43:20 -0800

    Renamed package

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "RimiFirebaseRTDBService",
+    name: "RimiFirebaseServices",
     platforms: [
         .iOS(.v17)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "RimiFirebaseRTDBService",
-            targets: ["RimiFirebaseRTDBService"]),
+            name: "RimiFirebaseServices",
+            targets: ["RimiFirebaseServices"]),
     ],
     dependencies: [
             // Dependencies declare other packages that this package depends on.
@@ -23,7 +23,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RimiFirebaseRTDBService",
+            name: "RimiFirebaseServices",
             dependencies: [
                 .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseDatabase", package: "firebase-ios-sdk"),
@@ -32,7 +32,7 @@ let package = Package(
         ),
         .testTarget(
             name: "RimiFirebaseRTDBServiceTests",
-            dependencies: ["RimiFirebaseRTDBService"]
+            dependencies: ["RimiFirebaseServices"]
         ),
     ]
 )
